928f8a120ba21374ba511f9482e2d9939bf27963
TheDescartes <1875311592@qq.com>
2023-07-31 17:36:51 +0800

    change-com

--- js/jd.js
+++ js/jd.js
@@ -1,4 +1,4 @@
-// 模糊查询
+// 模糊查询--
 
 //获取输入框
 let keyword = document.querySelector(".keyword");
@@ -111,25 +111,91 @@ for (let i = 0; i < lis.length; i++) {
 
 // 楼层定位-------------------------------
 
-let top_nav = document.querySelector(".top-nav");
-let nav = document.querySelector(".nav");
-let banner = document.querySelector(".banner");
+let t = document.querySelector(".top");//30px
+let nav = document.querySelector(".nav");       //140px
+let banner = document.querySelector(".banner"); //480px+button:30px
 let e = document.querySelector(".e");
+
+//包括height,pading,border////使用 clientHeight 属性返回元素的可见高度和宽度，仅包括内边距padding
+let tHeight = t.offsetHeight; //top-nav的高度
+
+let navHeight = nav.offsetHeight;//获取header的高读
+
+let bannerHeight = banner.offsetHeight;//banner的高读
+
+
+
+//楼层滚动文字改变颜色---------------------
+let ad = document.querySelector(".ad"); //囧东秒杀
+let adHeight = ad.offsetHeight;
+let content = document.querySelector(".content"); //频道广场
+let contentHeight = content.offsetHeight;
+let recommend = document.querySelector(".recommend"); //为你推荐
+let recHeight = recommend.offsetHeight;
+
+
+
+let ea = document.querySelectorAll(".e-a")       //获取所有的a,改变颜色
+let topE = bannerHeight + navHeight + tHeight + 30;      //基础高度
+let eArr = [topE, topE + adHeight, topE + contentHeight, recHeight + topE];//数组存各个div的高度
+console.log(ea[3]);
 
 document.onscroll = function () {
     //获取滚动条滚动乐多少高度
     let top = document.documentElement.scrollTop || document.body.scrollTop;
 
-    //获取header的高读
-    let navHeight = nav.offsetHeight;       //包括height,pading,border
-
-    let bannerHeight = banner.offsetHeight;
-
 
     //当滚动条滚动到一定程序，将楼层高度改为固定定位 
-    if (top >= 440) {
+    if (top >= topE) {
         e.className = "e e-fix";
     } else {
         e.className = "e";
     }
+    //------------清除颜色的函数--------
+    function clearColor() {
+        for (let i = 0; i < eArr.length; i++) {
+            ea[i].style.color = "";
+
+        }
+    }
+
+    //改变颜色
+    if (top > eArr[0] && top < eArr[1]) {
+        clearColor();
+        ea[0].style.color = "red";
+    } else if (top > eArr[1] && top < eArr[2]) {
+        clearColor();
+        ea[1].style.color = "red";
+    } else if (top > eArr[2]) {
+        clearColor();
+        ea[2].style.color = "red";
+    }
+    // console.log(top);
+    // console.log(eArr[2]);
+    // console.log(ea[3]);
+    // console.log(top < 3693);
+
+    //---------------头部吸顶效果-------------------
+    let search = document.querySelector(".search")
+    let searchM = document.querySelector(".search-content")
+    let form = document.querySelector(".search-bar")
+    let shop = document.querySelector(".shop")
+    let search_logo = document.querySelector(".search-logo")
+    console.log(search);
+    if (top >= topE) {
+        search.className = "search search-fix";//覆盖样式，优先级后写的生效
+        searchM.style.height = "50px";
+        form.style.top = "8px";
+        shop.style.top = "8px";
+        search_logo.style.display = "block";
+    } else {
+        search.className = "search";
+        searchM.style.height = "60px";
+        form.style.top = "25px";
+        shop.style.top = "25px";
+        search_logo.style.display = "inline";
+    }
 }
+
+
+
